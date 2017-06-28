9e86a19a3d44f4c7cf37bee17d6fcea13dbfd710
mixelpixel <mixelpix@mac.com>
2017-06-28 14:21:52 -0400

    importing img src and adding to body

--- profile/src/Body.js
+++ profile/src/Body.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'; // <--- no dot slash means look in node modules folder
 // import './Body.css';
+import Snoopy from './SnoopyPuppy.gif'
 
 export default class Body extends Component {
   render() {
     return (
        // class vs className, somewhat different attributes
       <div className="body">
-
+        <img src={Snoopy} alt="missing" />
       </div>
     );
   }
