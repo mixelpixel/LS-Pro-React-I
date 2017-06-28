793e9ef30294c59a7ebe418657eeadcdd2d68e2e
mixelpixel <mixelpix@mac.com>
2017-06-28 14:16:49 -0400

    html body

---
+++ profile/src/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; // <--- no dot slash means look in node modules folder
+// import './Body.css';
+
+export default class Body extends Component {
+  render() {
+    return (
+       // class vs className, somewhat different attributes
+      <div className="body">
+
+      </div>
+    );
+  }
+}
