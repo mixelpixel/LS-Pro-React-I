75dc556c0cba47244ddc3046b68b6c264267205d
mixelpixel <mixelpix@mac.com>
2017-06-27 23:29:43 -0400

    Nav Bar basic

---
+++ profile/src/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'; // <--- no dot slash means look in node modules folder
+// import NavBarButton from './NavBarButton';
+import './NavBar.css'; // <--- works with className="navbar"
+
+export default class NavBar extends Component {
+  render() {
+    return (
+       // class vs className, somewhat different attributes
+      <div className="navbar">
+        {/* <NavBarButton text={'Home'} />
+        <NavBarButton text={'FAQ'} />
+        <NavBarButton text={'LogIn'} />
+        <NavBarButton text={'About'} /> */}
+        {/* <h1>a title</h1> */}
+      </div>
+    );
+  }
+}
