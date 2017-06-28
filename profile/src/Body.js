95ecdca9d5c3b82306b6f75763c25c1612754608
mixelpixel <mixelpix@mac.com>
2017-06-28 14:25:50 -0400

    importing CSS

--- profile/src/Body.js
+++ profile/src/Body.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; // <--- no dot slash means look in node modules folder
-// import './Body.css';
+import './Body.css';
 import Snoopy from './SnoopyPuppy.gif'
 
 export default class Body extends Component {
@@ -8,6 +8,7 @@ export default class Body extends Component {
        // class vs className, somewhat different attributes
       <div className="body">
         <img src={Snoopy} alt="missing" />
+        Hello World!
       </div>
     );
   }
